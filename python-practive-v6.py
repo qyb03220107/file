6f6d33d913f206112e803624d2533250a1e07df8
zhengzichen <qyb03220107@163.com>
2018-11-06 18:56:32 +0800

    备份文件 版本更新

--- python-practive-v6.py
+++ python-practive-v6.py
@@ -1,15 +1,26 @@
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 import os
 import sys
-import  time
+import time
 
-source = os.makedirs('/home/swaroop/byte','/home/swaroop/bin')
-target_dir =  os.makedirs('/mnt/e/backup')
-target =  target_dir + time.strftime('%Y%m%d%H%M%S') + '.zip'
-zip_command = "zip -qr '%s' %s" % ('target, ''.join(source)')
+#try:
+source = [ '/home/swaroop/byte','/home/swaroop/bin']
+#except OSError:
+#    pass
+target_dir = ('/mnt/e/backup')
+today = target_dir + time.strftime("-%Y%m%d")
+now = time.strftime("H%M%S")
+if not os.path.exists(now):
+    try:
+        os.mkdir(today)
+    except OSError:
+        pass
+    print("Successfuly created directory ",today)
+print (target_dir)
+target = today +  os.sep  +  now  + '.zip'
+zip_command = "zip -qr '%s' %s" % (target, ' '.join(source))
 if os.system(zip_command) == 0:
-    print('Squccess backup to',target)
+    print ('Squccess backup to', target)
 else:
     print('BAKCUP FAILED')
-
-
