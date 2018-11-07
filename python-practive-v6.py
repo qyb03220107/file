ad5551586e448c084c15f2aa1c05794bc6b01552
zhengzichen <qyb03220107@163.com>
2018-11-07 22:29:04 +0800

    添加时间的判断

--- python-practive-v6.py
+++ python-practive-v6.py
@@ -8,23 +8,21 @@ import time
 source = [ '/home/swaroop/byte','/home/swaroop/bin']
 #except OSError:
 #    pass
-target_dir = ('/mnt/e/backup')
-today = target_dir + time.strftime("-%Y%m%d")
-comment = input('Enter a comment -->')
+target_dir = ('/mnt/e/backup/')
+today = target_dir + time.strftime("%Y%m%d")
 now = time.strftime("H%M%S")
+comment = input('Enter a comment --> ')
 #添加时间的判断
 if len(comment) == 0:
     target = today + os.sep + now + '.zip'
 else:
-    target = today + os.sep + now  + '_' + comment.replace('','_') + '.zip'
+    target = today + os.sep + now  + '_' + comment.replace(' ','_') + '.zip'
 if not os.path.exists(now):
     try:
         os.mkdir(today)
     except OSError:
         pass
     print("Successfuly created directory ",today)
-print (target_dir)
-target = today +  os.sep  +  now  + '.zip'
 zip_command = "zip -qr '%s' %s" % (target, ' '.join(source))
 if os.system(zip_command) == 0:
     print ('Squccess backup to', target)
