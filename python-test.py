bdd81321589aaef9d900aa5651628cd73a558f76
zhengzichen <qyb03220107@163.com>
2018-12-02 13:11:05 +0800

    python 还原文件

--- python-test.py
+++ python-test.py
@@ -17,7 +17,7 @@ if len(sys.argv) < 2:
     print('No action apecified')
     sys.exit()
 if sys.argv[1].startswith('--'):
-    option = sys.argv[1][2]
+    option = sys.argv[1][2:]
     if  option == 'verion':
         print('Version 1.2')
     elif option == 'help':
@@ -30,17 +30,3 @@ if sys.argv[1].startswith('--'):
 else:
     for filename in sys.argv[1:]:
         readfile(filename)
-if sys.argv[1].startswith('--'):
-    option = sys.argv[1][2:]
-    # fetch sys.argv[1] but without the first two characters
-    if option == 'version':
-        print ('Version 1.2')
-    elif option == 'help':
-        print ('''\
-       This program prints files to the standard output.
-       Any number of files can be specified.
-       Options include:
-        --version : Prints the version number
-         --help : Display this help''')
-    else:
-        print ('Unknown option.')
